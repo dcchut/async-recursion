cbc06e6a14a841105327ebeeb26b3780139b0016
Robert Usher <266585+dcchut@users.noreply.github.com>
2024-03-16 22:16:13 -0400

    Use visitor to collect references

--- tests/lifetimes.rs
+++ tests/lifetimes.rs
@@ -36,8 +36,9 @@ async fn contains_value_2<'a, 'b, T: PartialEq>(value: &'b T, node: &'b Node<'a,
 
 // The reference inside foo needs a `async_recursion bound
 #[async_recursion]
-async fn count_down(foo: Option<&str>) {
+async fn count_down(foo: Option<&str>) -> i32 {
     let _ = foo;
+    0
 }
 
 #[test]
@@ -70,5 +71,7 @@ fn lifetime_expansion_works() {
         assert_eq!(contains_value_2(&17, &node).await, true);
         assert_eq!(contains_value_2(&13, &node).await, true);
         assert_eq!(contains_value_2(&12, &node).await, false);
+
+        count_down(None).await;
     });
 }
